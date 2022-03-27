100a8b66f6e8feab09f1de4230ebf1b8d2416823
João Lucas Freitas <joao.frhb@gmail.com>
2022-03-27 20:44:36 -0300

    first commit

---
+++ components/Form.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="form">
+        <b-form-group
+            label-for="name"
+        >
+            <b-form-input
+                id="name"
+                v-model="form.name"
+                type="text"
+                placeholder="Seu nome"
+                required
+            />
+        </b-form-group>
+
+        <b-form-group
+            label-for="idade"
+        >
+            <b-form-input
+                id="idade"
+                v-model="form.age"
+                type="text"
+                placeholder="Sua idade"
+                required
+            />
+        </b-form-group>
+
+        <b-button 
+            type="submit" 
+            variant="secondary"
+            class="btn-submit"
+            @click="onSubmit"
+        >
+            Confirmar
+        </b-button>
+        
+        <b-button 
+            type="reset" 
+            variant="warning"
+            @click="onReset"
+        >
+            Limpar
+        </b-button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                name: '',
+                age: '',
+            }
+        }
+    },
+    methods: {
+        onSubmit() {
+            console.log("On Submit")   
+            this.form.name = ''
+            this.form.age = '' 
+        },
+        onReset() {
+            console.log("On reset")    
+        }
+    }
+    
+}
+</script>
+
+<style lang="css">
+    .form {
+        max-width: 80%;
+        margin: 0 auto;
+        text-align: center;
+    }
+    .btn-submit, .btn-submit:after {
+        background-color: #333;
+        outline: none;
+    }
+</style>
---
+++ components/Home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="content">
+        <h3>Jere e Rayssa</h3>      
+        <b-img 
+            src="../css/images/casal.jpeg" 
+            fluid 
+            alt="Responsive image" 
+            rounded="circle"
+            class="image-casal"
+        /> <br>
+        <b-img
+            src="../css/images/line.png"
+            class="image-divisor"
+        />
+        <br>
+        <br>
+        <div class="divisor">
+            <h5>Confirme sua Presença</h5>
+        </div>
+        
+        <div class="divisor">
+            <Form />
+        </div>
+
+        <!-- <div class="divisor">
+            <p class="text">
+                Neste dia especial queremos convidar você para
+                nossa celebração!
+            </p>
+            <p class="text">
+                <b>31/07/2022</b>
+            </p>
+        </div> -->
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css">
+    @font-face {
+        font-family: josefin;
+        src: url("../css/font/josefin-sans-thin.ttf");
+    }
+    .content {
+        margin: 0 auto;
+        margin-top: 5%;
+        text-align: center;
+        max-width: 600px;
+        font-family: 'josefin', sans-serif;
+    }
+    .divisor {
+        margin-top: 3%;
+    }
+    .image-divisor {
+        width: 120px;
+    }
+    .text {
+        max-width: 50%;
+        margin: 0 auto;
+        text-align: center;
+    }
+    .image-casal {
+        width: 120px;
+    }
+</style>
