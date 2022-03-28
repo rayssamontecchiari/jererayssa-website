501306f0d1bdff0fd6d549201ad06f28c61efc8e
Rayssa M <rayssamontecchiari00@gmail.com>
2022-03-27 21:26:11 -0300

    altera titulo

--- components/Home.vue
+++ components/Home.vue
@@ -1,28 +1,26 @@
 <template>
-    <div class="content">
-        <h3>Jere e Rayssa</h3>      
-        <b-img 
-            src="../css/images/casal.jpeg" 
-            fluid 
-            alt="Responsive image" 
-            rounded="circle"
-            class="image-casal"
-        /> <br>
-        <b-img
-            src="../css/images/line.png"
-            class="image-divisor"
-        />
-        <br>
-        <br>
-        <div class="divisor">
-            <h5>Confirme sua Presença</h5>
-        </div>
-        
-        <div class="divisor">
-            <Form />
-        </div>
+  <div class="content">
+    <h3>Rayssa e Jere</h3>
+    <b-img
+      src="../css/images/casal.jpeg"
+      fluid
+      alt="Responsive image"
+      rounded="circle"
+      class="image-casal"
+    />
+    <br />
+    <b-img src="../css/images/line.png" class="image-divisor" />
+    <br />
+    <br />
+    <div class="divisor">
+      <h5>Confirme sua Presença</h5>
+    </div>
 
-        <!-- <div class="divisor">
+    <div class="divisor">
+      <Form />
+    </div>
+
+    <!-- <div class="divisor">
             <p class="text">
                 Neste dia especial queremos convidar você para
                 nossa celebração!
@@ -31,40 +29,37 @@
                 <b>31/07/2022</b>
             </p>
         </div> -->
-    </div>
+  </div>
 </template>
 
-
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="css">
-    @font-face {
-        font-family: josefin;
-        src: url("../css/font/josefin-sans-thin.ttf");
-    }
-    .content {
-        margin: 0 auto;
-        margin-top: 5%;
-        text-align: center;
-        max-width: 600px;
-        font-family: 'josefin', sans-serif;
-    }
-    .divisor {
-        margin-top: 3%;
-    }
-    .image-divisor {
-        width: 120px;
-    }
-    .text {
-        max-width: 50%;
-        margin: 0 auto;
-        text-align: center;
-    }
-    .image-casal {
-        width: 120px;
-    }
+@font-face {
+  font-family: josefin;
+  src: url("../css/font/josefin-sans-thin.ttf");
+}
+.content {
+  margin: 0 auto;
+  margin-top: 5%;
+  text-align: center;
+  max-width: 600px;
+  font-family: "josefin", sans-serif;
+}
+.divisor {
+  margin-top: 3%;
+}
+.image-divisor {
+  width: 120px;
+}
+.text {
+  max-width: 50%;
+  margin: 0 auto;
+  text-align: center;
+}
+.image-casal {
+  width: 120px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
-  <!-- <Tutorial/> -->
   <Home />
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
